64c118bbadc0dfc71546a3b3a8aff19587ede00f
warrickbayman <me@warrickbayman.co.za>
2020-10-06 21:01:33 +0200

    wip

--- tests/Unit/ServerTest.php
+++ tests/Unit/ServerTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace TPG\Attache\Tests\Unit;
 
+use Illuminate\Support\Arr;
 use League\Flysystem\Adapter\Local;
 use League\Flysystem\Filesystem;
 use TPG\Attache\Initializer;
@@ -14,7 +15,7 @@ class ServerTest extends TestCase
     /**
      * @test
      */
-    public function it_can_store_a_server_cnofig()
+    public function it_can_store_a_server_config(): void
     {
         $filesystem = new Filesystem(new Local(__DIR__));
         $initializer = new Initializer($filesystem);
@@ -27,18 +28,19 @@ class ServerTest extends TestCase
         $this->assertSame('user', $server->user());
         $this->assertSame('/path/to/application', $server->root());
         $this->assertSame('/path/to/application/', $server->root(true));
-        $this->assertTrue($server->migrate());
+        $this->assertSame('master', $server->branch());
+        $this->assertFalse($server->migrate());
     }
 
     /**
      * @test
      */
-    public function it_can_get_the_configured_paths()
+    public function it_can_get_the_configured_paths(): void
     {
         $filesystem = new Filesystem(new Local(__DIR__));
         $initializer = new Initializer($filesystem);
 
-        $config = $initializer->defaultConfig('remote..com')['servers']['production'];
+        $config = $initializer->defaultConfig('remote.com')['servers']['production'];
 
         $server = new Server('production', $config);
         $root = $server->root(true);
@@ -47,5 +49,52 @@ class ServerTest extends TestCase
         $this->assertSame($root.'live', $server->path('serve'));
         $this->assertSame($root.'storage', $server->path('storage'));
         $this->assertSame($root.'.env', $server->path('env'));
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_have_a_set_php_bin_config()
+    {
+        $filesystem = new Filesystem(new Local(__DIR__));
+        $initializer = new Initializer($filesystem);
+
+        $config = Arr::get($initializer->defaultConfig('remote.com'), 'servers.production');
+        Arr::set($config, 'php.bin', '/path/to/php/bin');
+
+        $server = new Server('production', $config);
+        $this->assertSame('/path/to/php/bin', $server->php());
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_have_a_local_composer_path()
+    {
+        $filesystem = new Filesystem(new Local(__DIR__));
+        $initializer = new Initializer($filesystem);
+
+        $config = Arr::get($initializer->defaultConfig('remote.com'), 'servers.production');
+        Arr::set($config, 'composer', [
+            'bin' => 'composer-bin.phar',
+            'local' => true,
+        ]);
+
+        $server = new Server('production', $config);
+        $this->assertSame('/path/to/application/composer-bin.phar', $server->composer());
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_return_the_ssh_connection_string()
+    {
+        $filesystem = new Filesystem(new Local(__DIR__));
+        $initializer = new Initializer($filesystem);
+
+        $config = Arr::get($initializer->defaultConfig('remote.com'), 'servers.production');
+        $server = new Server('production', $config);
+
+        $this->assertSame('user@example.test -p22', $server->connectionString());
     }
 }
