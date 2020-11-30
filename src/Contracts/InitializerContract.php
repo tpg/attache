448fee01040eada0ba4714b42703b94513532a31
Warrick Bayman <me@warrickbayman.com>
2020-11-30 10:07:17 +0200

    Apply fixes from StyleCI (#61)
    [ci skip] [skip ci]

--- src/Contracts/InitializerContract.php
+++ src/Contracts/InitializerContract.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace TPG\Attache\Contracts;
@@ -9,9 +10,14 @@ use League\Flysystem\Filesystem;
 interface InitializerContract
 {
     public function __construct(Filesystem $filesystem);
+
     public function create(string $filename, string $gitRemote): void;
+
     public function save(string $filename, array $config): void;
+
     public function discoverGitRemotes(): Collection;
+
     public function defaultConfig(string $gitRemote): array;
+
     public function defaultServerConfig(): array;
 }
--- src/Contracts/ReleaseManagerContract.php
+++ src/Contracts/ReleaseManagerContract.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace TPG\Attache\Contracts;
@@ -10,8 +11,12 @@ use TPG\Attache\TaskRunner;
 interface ReleaseManagerContract
 {
     public function setTarget(Target $target): void;
+
     public function setTaskRunner(TaskRunner $runner): void;
+
     public function list(): Collection;
+
     public function active(): string;
+
     public function activate(string $release): bool;
 }
