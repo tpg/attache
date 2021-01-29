cc3f4613eb7cfc5290aa39b5ff98c2d964afb87d
Warrick Bayman <warrickbayman@users.noreply.github.com>
2021-01-29 21:42:42 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Contracts/InitializerInterface.php
+++ src/Contracts/InitializerInterface.php
@@ -7,6 +7,8 @@ namespace TPG\Attache\Contracts;
 interface InitializerInterface
 {
     public function create(array $config, string $filename = '.attache.json');
+
     public function discoverGitRemotes(): array;
+
     public function config(string $remote): array;
 }
